ea01f0e6453b80bb659e0ce8fa554dc7771b4565
rubendediego90 <rubendediego90@gmail.com>
2026-03-12 12:13:37 +0100

    quitar mas mierda

--- src/router/routes/about.route.js
+++
@@ -1,5 +0,0 @@
-export default {
-  path: '/about',
-  name: 'About',
-  component: () => import('../../views/About.vue')
-};
--- src/router/routes/home.route.js
+++
@@ -1,9 +0,0 @@
-import { routes} from '../../constants/routes.constants';
-export default {
-  path: routes.DEFAULT,
-    name: 'Home',
-    component: () => import("../../views/Home.vue"),
-    meta: {
-      requiresAuth: true
-    }
-  };
